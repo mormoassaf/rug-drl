1beee11ec4f05a8f6cc113b61fd7ec6e7d0c2ad5
Mo Assaf <mormoassaf@gmail.com>
2023-05-14 17:07:07 +0200

    prepared DQN

--- catchgame/modules/dqn/network.py
+++ catchgame/modules/dqn/network.py
@@ -22,7 +22,7 @@ class QNetwork(torch.nn.Module):
 
 class QNetworkConv(torch.nn.Module):
 
-    def __init__(self, in_channels, spatial_size, output_size):
+    def __init__(self, in_channels: int, spatial_size: tuple, output_size: int):
         super(QNetworkConv, self).__init__()
         self.in_channels = in_channels
         self.spatial_size = spatial_size
@@ -31,12 +31,25 @@ class QNetworkConv(torch.nn.Module):
         self.conv1 = nn.Conv2d(self.in_channels, 32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3)
-        self.conv4 = nn.Conv2d(64, 512, kernel_size=1)
-        self.conv5 = nn.Conv2d(512, self.output_size, kernel_size=1)
 
+        conv_out_size = self._get_conv_out_size()
+        # output should be (batch, output_size)
+        self.fc1 = nn.Linear(conv_out_size, 512)
+        self.fc2 = nn.Linear(512, self.output_size)
+
+    def _get_conv_out_size(self):
+        x = torch.zeros(1, self.in_channels, *self.spatial_size)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        return x.numel()
+    
     def forward(self, x):
+        x = x.float() / 255
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))        
-        x = F.relu(self.conv4(x))
-        return self.conv5(x)
+        x = F.relu(self.conv3(x))
+
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
